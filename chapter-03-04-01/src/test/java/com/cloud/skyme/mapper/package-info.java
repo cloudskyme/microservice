0b10d6b11b9d6dbb6d5ad3ffd0cd16e5fc126488
zhangfeng <zhangfeng@DESKTOP-E3I9LR5>
2017-10-25 18:02:02 +0800

    添加单数据源整合mybatis

---
+++ chapter-03-04-01/src/test/java/com/cloud/skyme/mapper/package-info.java
@@ -0,0 +1,35 @@
+/******************************************************************
+ *
+ *    Copyright (c) 2010-2017 Digital Telemedia Co.,Ltd
+ *    https://home.cnblogs.com/u/skyme/
+ *
+ *    Package:     com.cloud.skyme.mapper
+ *
+ *    Filename:    package-info.java
+ *
+ *    Description: CRUD测试
+ *
+ *    Copyright:   Copyright (c) 2010-2017
+ *
+ *    Company:     cloudskyme
+ *
+ *    @author:     zhangfeng
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2017年10月25日 下午5:08:28
+ *
+ *    Revision:
+ *
+ *    2017年10月25日 下午5:08:28
+ *        - first revision
+ *
+ *****************************************************************/
+/**
+ * @ClassName package-info
+ * @Description CRUD测试
+ * @author zhangfeng
+ * @Date 2017年10月25日 下午5:08:28
+ * @version 1.0.0
+ */
+package com.cloud.skyme.mapper;
