21e9892ac6a5e8297f1562a39d76d76b7d42c228
zhangfeng <zhangfeng@DESKTOP-E3I9LR5>
2017-11-08 11:29:40 +0800

    spring boot整合web

---
+++ chapter-03-05/src/main/java/com/cloud/skyme/controller/package-info.java
@@ -0,0 +1,35 @@
+/******************************************************************
+ *
+ *    Copyright (c) 2010-2017 Digital Telemedia Co.,Ltd
+ *    https://home.cnblogs.com/u/skyme/
+ *
+ *    Package:     com.cloud.skyme.controller
+ *
+ *    Filename:    package-info.java
+ *
+ *    Description: controller调用
+ *
+ *    Copyright:   Copyright (c) 2010-2017
+ *
+ *    Company:     cloudskyme
+ *
+ *    @author:     zhangfeng
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2017年11月8日 上午9:41:21
+ *
+ *    Revision:
+ *
+ *    2017年11月8日 上午9:41:21
+ *        - first revision
+ *
+ *****************************************************************/
+/**
+ * @ClassName package-info
+ * @Description controller调用
+ * @author zhangfeng
+ * @Date 2017年11月8日 上午9:41:21
+ * @version 1.0.0
+ */
+package com.cloud.skyme.controller;
