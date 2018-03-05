f028bbf6c3bbac170fc5b4d42a647ef4af8f23a1
zhangfeng <zhangfeng@DESKTOP-E3I9LR5>
2018-03-05 19:20:12 +0800

    spring boot dubbo服务提供者

--- chapter-03-09-provider/src/main/java/com/cloud/skyme/Chapter0309ProviderApplication.java
+++ chapter-03-09-provider/src/main/java/com/cloud/skyme/Chapter0309ProviderApplication.java
@@ -3,6 +3,13 @@ package com.cloud.skyme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @ClassName Chapter0309ProviderApplication
+ * @Description dubbo服务提供者
+ * @author zhangfeng
+ * @Date 2018年3月5日 下午7:10:40
+ * @version 1.0.0
+ */
 @SpringBootApplication
 public class Chapter0309ProviderApplication {
 
