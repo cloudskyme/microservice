5de6ba128df0e488c62ef447019109ca7e1b2ead
zhangfeng <zhangfeng@DESKTOP-E3I9LR5>
2018-03-05 19:06:10 +0800

    健康是永恒的主题

--- chapter-03-08/src/main/java/com/cloud/skyme/Chapter0308Application.java
+++ chapter-03-08/src/main/java/com/cloud/skyme/Chapter0308Application.java
@@ -3,6 +3,13 @@ package com.cloud.skyme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @ClassName Chapter0308Application
+ * @Description 健康检查
+ * @author zhangfeng
+ * @Date 2018年3月5日 下午7:01:33
+ * @version 1.0.0
+ */
 @SpringBootApplication
 public class Chapter0308Application {
 
