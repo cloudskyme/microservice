d13c74ff1592fa06808099547e8dbc416e55f830
zhangfeng <zhangfeng@DESKTOP-E3I9LR5>
2018-03-06 07:47:48 +0800

    多数据源

--- chapter-03-04-02/src/main/java/com/cloud/skyme/datasource/DataSourceConfig.java
+++ chapter-03-04-02/src/main/java/com/cloud/skyme/datasource/DataSourceConfig.java
@@ -29,8 +29,8 @@ package com.cloud.skyme.datasource;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
